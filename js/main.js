d35254eb2b6c46025150e50c4dacbeb71d282ce7
bearded.qa <bearded.qa@gmail.com>
2019-07-23 20:29:48 +0200

    js changed for nicer mobile view

--- js/main.js
+++ js/main.js
@@ -8,13 +8,8 @@ $(document).ready(function () {
   $('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
-    }
+      $('.content-wrapper').addClass('animated slideInUp slideInRight')
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
